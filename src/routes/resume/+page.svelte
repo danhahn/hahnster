acf2df9a2e2ecd2c8ce76502c1ad11d2ddbb6ccb
Dan Hahn <daniel.hahn@onepeloton.com>
2023-03-01 17:11:53 -0500

    Add resume page

---
+++ src/routes/resume/+page.svelte
@@ -0,0 +1,214 @@
+<script>
+	import Headline from '$lib/Headline.svelte';
+	import Section from '$lib/Section.svelte';
+	import TextLayout from '$lib/TextLayout.svelte';
+</script>
+
+<Section>
+	<Headline>Resume</Headline>
+	<TextLayout variation="narrow">
+		<h2>Key Skills</h2>
+		<p>
+			ReactJs, Redux, Javascript, TypeScript ,Svelte, Storybook, GraphQL, Gatsby, ES6, HTML5, CSS3,
+			LESS/SCSS, MySql, Firebase, NodeJs, Git, Scrum.
+		</p>
+		<h2>Experience</h2>
+		<h3>Sr. Software Engineer - E-Commerce</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Peloton, New York, NY</p>
+			<p class="italic text-sm">5/2020 - Present</p>
+		</div>
+		<p>Peloton is a global leader in connected fitness.</p>
+		<ul>
+			<li>
+				Currently working on building out our design system and component library using Storybook,
+				React and styled components.
+			</li>
+			<li>
+				Work very closely with the design team to ensure standard terminology, design tokens,
+				component design
+			</li>
+			<li>
+				Developer advocate for the design system teaching internal developers new patterns and best
+				practices
+			</li>
+			<li>
+				Feature lead for accessibility work (A11y) and ensuring that external audit was completed
+				with a passing grade.
+			</li>
+			<li>
+				Ran many lunch and learns to teach other developers why and how to ensure that future work
+				is fully accessible.
+			</li>
+			<li>
+				Feature Lead for lunch or marking pages for new Bike and Tread pages on onepeloton.com
+			</li>
+		</ul>
+		<h3>Sr. Software Engineer - E-Commerce</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Fevo, New York, NY</p>
+			<p class="italic text-sm">1/2019-3/2020</p>
+		</div>
+		<p>
+			Fevo is a social ticketing platform that allows for a new way to sell anything. Built using
+			React and GraphQL.
+		</p>
+
+		<ul>
+			<li>
+				Update the application to ensure that all public-facing applications are ADA compliant.
+			</li>
+			<li>Build and maintain global tracking for consumer application</li>
+			<li>Built a component library for consumer and b2b applications</li>
+			<li>Build new fevo.com using Gatsby</li>
+		</ul>
+
+		<h3>Sr. Software Engineer - E-Commerce</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Michael Kors, New York, NY</p>
+			<p class="italic text-sm">11/2016-1/2019</p>
+		</div>
+		<p>
+			michaelkors.com - Worldwide e-commerce platform for Michael Kors Inc. Over 1 million monthly
+			visitors and millions of dollars in sales annually in sales. Build as a headless application
+			using ReactJs and Redux using ES6.
+		</p>
+
+		<ul>
+			<li>Serve as an architect working with third-party contractors.</li>
+			<li>Integrate translations from a vendor into the codebase.</li>
+			<li>Phone screen and interview potential candidates.</li>
+		</ul>
+
+		<h3>Sr. Software Engineer</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">AppNexus, New York, NY</p>
+			<p class="italic text-sm">1/2012-10/2016</p>
+		</div>
+		<p>
+			AppNexus is an internet technology company that enables and optimizes the real-time sale and
+			purchase of digital advertising. Their powerful, real-time decision platform supports core
+			products that enable publishers to maximize yield; and marketers and agencies to harness data
+			and machine learning to deliver intelligent and customized campaigns
+		</p>
+		<p>
+			Working with ReactJs build and maintain components used by AppNexus core application.
+			Responsible for all aspects of development including JSX templates, Moca test, and CSS/LESS
+			styles. Contributed over 50 core components to a shared library.
+		</p>
+		<p>
+			Contributed to AppNexus open-source ReactJs library Lucid-IU. Built many of the core
+			components include the Grid Table, CSS/LESS variables, core animations.
+		</p>
+		<p>
+			Using EmberJs worked on a project to allow traders of the application to view their
+			information and make changes to speed up user's workflow.
+		</p>
+		<p>
+			Lead a team, the design architects whose responsibility was to develop deploy and maintain all
+			HTML, CSS components within the application.
+		</p>
+		<p>Team was made up of 5 engineers</p>
+		<p>
+			Managed career growth, including quarterly reviews, weekly syncs and day to day task
+			management.
+		</p>
+		<h3>Continuing Education Professor</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">School of Visual Arts, New York, NY</p>
+			<p class="italic text-sm">6/2002-2020</p>
+		</div>
+		<p>Teach four night/weekend counting education class at SVA.</p>
+		<p>Introduction to Web Languages: HTML (4.6 out of 5 student rating)</p>
+		<p>Introduction to Web Languages: Cascading Style Sheets (4.8 out of 5 student rating)</p>
+		<p>Weekend HTML (4.6 out of 5 student rating)</p>
+		<p>Weekend Advanced HTML (4.7 out of 5 student rating)</p>
+
+		<h3>Senior Front End Developer</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Starwood Hotels and Resorts Worldwide, White Plains, NY</p>
+			<p class="italic text-sm">4/2006-6/2009</p>
+		</div>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Accenture</p>
+			<p class="italic text-sm">6/2009-2/2012</p>
+		</div>
+		<p>
+			Starwood Hotels has over 1000 properties in over 100 countries. Starwood Hotels has nine
+			different brands under its umbrella including Sheraton, Westin, St. Regis, W Hotels, and Four
+			Points. Since joining Starwood in 2006 the branded websites revenue has grown from just over 1
+			billion dollars to well over 2 billion dollars in 2009. Starwood's brands each have their own
+			branded website that represents each brand's values. All the branded website share a central
+			reservations system. Starwood being a global company has its branded website translated into
+			seven different languages. In the spring of 2009 Starwood entered into a partnership with
+			Accenture where Starwood would move the development of the websites to Accenture. It was at
+			this time my job was moved from Starwood to Accenture. I have remained in the same role but
+			taking on much more responsibilities working with the offshore team.
+		</p>
+		<ul>
+			<li>Fourpoints.com Redesign – Lead the redesign of the fourpoints.com site.</li>
+			<li>Enhancement Manager - Manage 10 to 12 enhancements per release</li>
+			<li>Stregis.com Redesign – Lead the redesign of the stregis.com site.</li>
+			<li>
+				Property page rewrite – Converted common JSP for all property pages on all of Starwood’s
+				sites. This change allows for future development time to be cut in half. Since all brands
+				are working off same base CSS file the display of each page in the brand skin is more
+				constant.
+			</li>
+			<li>
+				Sheraton.com Redesign – Sheraton.com averages over 1 million dollars a day in reservations.
+				The redesign updated the branding of the site and had to launch at the same time as
+			</li>
+			<li>
+				Property Page Optimization - Convert to new CMS system to better inform users about details
+				of properties
+			</li>
+			<li>
+				Booking Path Migration - revised the booking path for all brands within the Starwood family
+				to use a new booking flow. Rewrote all markup and CSS.
+			</li>
+			<li>W Hotels Redesign - Lead developer on for branded website redesign.</li>
+		</ul>
+
+		<h3>Senior Interface Engineer</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Grey Interactive, New York NY</p>
+			<div>
+				<p class="italic text-sm">8/2001 – 3/2006,</p>
+				<p class="italic text-sm">9/1999 – 9/2000</p>
+			</div>
+		</div>
+		<p>M&M’S website</p>
+		<ul>
+			<li>Built current site including setting up general structure</li>
+			<li>Created a JavaScript library, including form, flash and browser validation.</li>
+			<li>Integrated site-wide style sheet.</li>
+			<li>Integrated JSP including registration, send to a friend and dynamic content.</li>
+			<li>Worked with JSP, Flash registration and send to a friend.</li>
+			<li>Create Mass market emails (viewed by over 1 million people)</li>
+			<li>Gerber.com</li>
+			<li>Built current site, template, and structure.</li>
+			<li>Integrate JSP login, registration, and Send to friends.</li>
+			<li>
+				Update current site on a weekly basis, including general updated, new pages and minis sites.
+			</li>
+		</ul>
+		<p>Covergirl.com</p>
+		<ul>
+			<li>Created a version of DHTML site navigation in 2000. (No longer in use).</li>
+			<li>Created mass email sent out to over 500,000 users.</li>
+		</ul>
+		<h3>Developer</h3>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Gameloft.com, New York NY</p>
+			<p class="italic text-sm">9/2000 - 5/2001</p>
+		</div>
+		<p>Lead developer on the gameloft.com community page.</p>
+		<h2>Education</h2>
+		<div class="md:flex justify-between">
+			<p class="text-lg font-bold">Marist College – Poughkeepsie, NY</p>
+			<p class="italic text-sm">5/1999</p>
+		</div>
+		<p>Bachelor of Arts Degree – Communications-Radio/TV/Film</p>
+	</TextLayout>
+</Section>
