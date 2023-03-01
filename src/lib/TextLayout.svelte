84483121cc294e767d16ac84c34401db9cfea966
Dan Hahn <daniel.hahn@onepeloton.com>
2023-03-01 17:12:23 -0500

    adjust spacing on text pages

--- src/lib/TextLayout.svelte
+++ src/lib/TextLayout.svelte
@@ -1,4 +1,10 @@
-<div class="page grid gap-4">
+<script lang="ts">
+	export let variation: 'normal' | 'narrow' = 'normal';
+</script>
+
+<div
+	class={`${variation} page grid ${variation === 'narrow' ? 'gap-2' : 'gap-4'} ${$$props.class}`}
+>
 	<slot />
 </div>
 
@@ -6,10 +12,19 @@
 	* :global(p) {
 		@apply leading-7 md:leading-8;
 	}
+	.narrow :global(p) {
+		@apply leading-5 md:leading-6;
+	}
 	* :global(h2) {
-		@apply text-2xl font-bold text-teal-700 dark:text-teal-200;
+		@apply text-2xl font-bold text-teal-700 dark:text-teal-200 my-4 md:my-6 uppercase;
+	}
+	* :global(h3) {
+		@apply text-xl font-bold text-teal-700 dark:text-teal-50 border-b border-teal-800 dark:border-teal-300 pb-2 md:mt-4;
 	}
 	:global(.page ul) {
 		@apply list-disc pl-4 md:pr-28 grid gap-2 leading-7 md:leading-8;
+	}
+	.narrow :global(ul) {
+		@apply list-disc pl-4 md:pr-28 grid gap-2 leading-5 md:leading-6;
 	}
 </style>
