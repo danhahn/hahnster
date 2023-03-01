eda0211d238240c88d5873138cf20382ad708009
Dan Hahn <daniel.hahn@onepeloton.com>
2023-03-01 16:13:42 -0500

    adjust spacing

--- src/lib/TextLayout.svelte
+++ src/lib/TextLayout.svelte
@@ -3,10 +3,13 @@
 </div>
 
 <style lang="postcss">
-	:global(h2) {
-		@apply text-2xl font-bold;
+	* :global(p) {
+		@apply leading-7 md:leading-8;
+	}
+	* :global(h2) {
+		@apply text-2xl font-bold text-teal-700 dark:text-teal-200;
 	}
 	:global(.page ul) {
-		@apply list-disc pl-4 grid gap-2;
+		@apply list-disc pl-4 md:pr-28 grid gap-2 leading-7 md:leading-8;
 	}
 </style>
