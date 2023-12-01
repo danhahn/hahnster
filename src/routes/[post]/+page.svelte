910d78e69bc02a436390ddc8a9cc9a66bec62ed2
Dan <danielhahn@gmail.com>
2023-12-01 16:03:58 -0500

    Add new page component for dynamic posts

---
+++ src/routes/[post]/+page.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import type { PageData } from './$types';
+
+	export let data: PageData;
+</script>
+
+<svelte:head>
+	<title>{data.meta.title}</title>
+	<meta property="og:type" content="article" />
+	<meta property="og:title" content={data.meta.title} />
+	<meta property="og:description" content={data.meta.description} />
+</svelte:head>
+
+<pre>
+  {JSON.stringify(data, null, 2)}
+</pre>
+
+<article>
+	<hgroup>
+		<h1>{data.meta.title}</h1>
+		<p>Published on {data.meta.date}</p>
+	</hgroup>
+
+	<div class="prose">
+		<svelte:component this={data.content} />
+	</div>
+</article>
