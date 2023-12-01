8b03f1ccd9eda9d5826ea2075e2422a02ac7b9b9
Dan <danielhahn@gmail.com>
2023-12-01 16:30:22 -0500

    Refactor card component styling

--- src/lib/Card.svelte
+++ src/lib/Card.svelte
@@ -7,79 +7,18 @@
 	export let linkText: string;
 </script>
 
-<div class="card flex flex-col bg-white rounded-lg shadow-xl shadow-black/20 overflow-hidden">
-	<div class="card__image">
+<div class="card bg-base-100 shadow-xl">
+	<figure>
 		<img src={image} alt={title} />
-	</div>
-	<div class="p-6">
-		<h3 class="card__title text-teal-600">{title}</h3>
+	</figure>
+	<div class="card-body">
+		<h3 class="card-title">{title}</h3>
 		{#if subtitle}
 			<h4 class="card__subtitle">{subtitle}</h4>
 		{/if}
-		<p class="card__description">{description}</p>
-		<a href={link} class="card__link" target="_blank" rel="noreferrer">{linkText}</a>
+		<p>{description}</p>
+		<div class="card-action justify-end">
+			<a href={link} class="btn btn-primary" target="_blank" rel="noreferrer">{linkText}</a>
+		</div>
 	</div>
 </div>
-
-<style lang="postcss">
-	.card {
-		transition: all 0.3s ease-in-out;
-	}
-
-	.card:hover {
-		transform: translateY(-0.25rem);
-		box-shadow: 0 1rem 2rem rgba(0, 0, 0, 0.15);
-	}
-
-	.card__image {
-		width: 100%;
-		height: 15rem;
-		overflow: hidden;
-	}
-
-	.card__image img {
-		width: 100%;
-		height: 100%;
-		object-fit: cover;
-		object-position: top;
-	}
-
-	.card__content {
-		padding: 1.5rem;
-	}
-
-	.card__title {
-		margin: 0 0 0.5rem 0;
-		font-size: 1.5rem;
-		font-weight: 700;
-	}
-
-	.card__subtitle {
-		margin: 0 0 1rem 0;
-		font-size: 1rem;
-		font-weight: 400;
-		color: #777;
-	}
-
-	.card__description {
-		margin: 0 0 1.5rem 0;
-		font-size: 1rem;
-		font-weight: 400;
-		color: #777;
-	}
-
-	.card__link {
-		display: inline-block;
-		padding: 0.5rem 1rem;
-		background-color: #000;
-		color: #fff;
-		border-radius: 0.25rem;
-		text-decoration: none;
-		transition: all 0.3s ease-in-out;
-	}
-
-	.card__link:hover {
-		transform: translateY(-0.25rem);
-		box-shadow: 0 0.5rem 1rem rgba(0, 0, 0, 0.15);
-	}
-</style>
