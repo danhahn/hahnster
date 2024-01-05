d07da54fd5ee2e78ac9451e9d13c766d2d4d7f48
Dan Hahn <danielhahn@gmail.com>
2024-01-04 22:55:42 -0500

    Update import path for app.css in +layout.svelte

--- src/routes/(site)/+layout.svelte
+++ src/routes/(site)/+layout.svelte
@@ -1,5 +1,5 @@
 <script>
-	import '../app.css';
+	import '../../app.css';
 	import Navigation from '$lib/Navigation.svelte';
 	import Footer from '$lib/Footer.svelte';
 	import { siteName } from '$lib';
